b8b310701277fa24f63f77af2dcd19e182101601
Crackvignoule <88386090+Crackvignoule@users.noreply.github.com>
2024-03-18 12:18:08 +0000

    added goofy chart

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+
+import App from './components/App';
+import chart from './components/Chart';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <chart />
   </React.StrictMode>
 );
 
