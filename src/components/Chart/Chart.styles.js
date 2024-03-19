970002dd5005b5430c8a049120e26b153680c1d9
Crackvignoule <kiki.kalagan@gmail.com>
2024-03-19 20:05:32 +0100

    restructure

---
+++ src/components/Chart/Chart.styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const ChartContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 400px;
+  width: 100%;
+  background-color: #f5f5f5;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.1);
+`;
+
+export const ChartTitle = styled.h2`
+  font-size: 1.5em;
+  text-align: center;
+  color: #333;
+`;
