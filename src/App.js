ed928e8178c4133f2c8191d5c3ac1d30644ff685
Crackvignoule <88386090+Crackvignoule@users.noreply.github.com>
2024-03-19 18:42:24 +0000

    Pending changes exported from your codespace

--- src/App.js
+++ src/App.js
@@ -1,37 +1,32 @@
 import GlobalStyles from './styles/GlobalStyles';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home, Login, Portfolio, Leaderboard } from './pages';
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <div>
-        {/* <Router>
+      <Router>
         <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/leaderboard">Leaderboard</Link>
-              </li>
-              <li>
-                <Link to="/portfolio">Portfolio</Link>
-              </li>
-            </ul>
-          </nav>
-
-          <Switch>
-            <Route path="/">
-              <HomePage />
-            </Route>
-            <Route path="/portfolio">
-              <Portfolio />
-            </Route>
-            <Route path="/leaderboard">
-              <Leaderboard />
-            </Route>
-          </Switch>
+          {/*
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/leaderboard">Leaderboard</Link>
+                </li>
+                <li>
+                  <Link to="/portfolio">Portfolio</Link>
+                </li>
+              </ul>
+            </nav>*/}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/portfolio" element={<Portfolio />}></Route>
+            <Route path="/leaderboard" element={<Leaderboard />} />
+          </Routes>
         </div>
-      </Router> */}
-      </div>
+      </Router>
     </>
   );
 }
