e3b67e8e3dfc8f02f0c61e58c661cfac1f24763d
Crackvignoule <kiki.kalagan@gmail.com>
2024-03-21 16:07:41 +0100

    now using advanced chart

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Chart from '../../components/Chart/Chart';
-import ChartOptions from '../../components/Chart/ToolBarChart/ToolBarOptions';
 import NavBar from '../../components/NavBar/NavBar';
 
 function Home() {
@@ -8,8 +7,7 @@ function Home() {
     return (
         <div>
           <NavBar />
-          <Chart/>
-          <ChartOptions/>
+          <Chart />
         </div>
       );
 }
