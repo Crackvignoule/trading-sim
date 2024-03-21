a2558dfda34d2413e210e2861230cfe3c260e3c4
Slynax <remlouplay3@gmail.com>
2024-03-21 10:14:47 +0100

    add Tool bar

---
+++ client/src/components/Chart/ToolBarChart/ToolBarOptions.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useRef, useState } from 'react';
+
+
+function ToolBarOptions() {
+
+
+return (
+    <ToolBarChart>
+    <Text>test<Text/>
+    <ToolBarChart/>
+);
+};
+
+export default ToolBarOptions;
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Chart from '../../components/Chart/Chart';
+import ChartOptions from '../../components/Chart/ToolBarChart/ToolBarOptions';
 import NavBar from '../../components/NavBar/NavBar';
 
 function Home() {
@@ -7,7 +8,8 @@ function Home() {
     return (
         <div>
           <NavBar />
-          <Chart key="unique-chart-key" />
+          <Chart/>
+          <ChartOptions/>
         </div>
       );
 }
