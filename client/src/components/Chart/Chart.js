a43815e992fcadf8353f9c1f9280b9c5a2e80132
Crackvignoule <kiki.kalagan@gmail.com>
2024-03-25 10:33:35 +0100

    chart wip

--- client/src/components/Chart/Chart.js
+++ client/src/components/Chart/Chart.js
@@ -22,12 +22,24 @@ function Chart() {
         allow_symbol_change: true,
         calendar: false,
         hide_side_toolbar: false,
+        watchlist: ["AAPL", "IBM", "TSLA", "AMD", "MSFT", "GOOG"],
         overrides: {
           "mainSeriesProperties.areaStyle.linecolor": "rgba(32, 226, 47, 1)",
           "mainSeriesProperties.areaStyle.color1": "rgba(32, 226, 47, 0.56)",
           "mainSeriesProperties.areaStyle.color2": "rgba(32, 226, 47, 0.04)",
         },
-      });
+        searchSymbols: async (
+          userInput,
+          exchange,
+          symbolType,
+          onResultReadyCallback,
+      ) => {
+          console.log('[searchSymbols]: Method call');
+          // const symbols = await getMatchingSymbolsFromBackend(userInput, exchange, symbolType);
+          // onResultReadyCallback(newSymbols);
+      }
+      }
+      );
     };
     chartContainerRef.current.appendChild(script);
   }, []);
@@ -38,53 +50,3 @@ function Chart() {
 }
 
 export default Chart;
-
-// OLD CODE
-// import React, { useEffect, useRef, useState } from 'react';
-// import createAreaChart from './AreaChart/AreaChart';
-// import { ChartContainer } from './Chart.styles';
-// import ToolBarChart from './ToolBarChart/ToolBarChart';
-
-// function Chart({ showToolbar = false }) {
-//     const chartContainerRef = useRef();
-//     const [series, setSeries] = useState(null);
-
-//     // Initialisation du graphique et chargement des données historiques
-//     useEffect(() => {
-//         console.log('Création du graphique');
-//         // Videz le conteneur avant de créer un nouveau graphique
-//         chartContainerRef.current.innerHTML = '';
-
-//         fetch('http://localhost:5000/api/chartBTCUSDT')
-//             .then(response => response.json())
-//             .then(data => {
-//                 const { chart, series } = createAreaChart(chartContainerRef.current, data);
-//                 setSeries(series);
-//             })
-//             .catch(error => console.error("Erreur lors de la récupération des données historiques:", error));
-//     }, []);
-
-//     // Écoute des mises à jour en temps réel via WebSocket
-//     useEffect(() => {
-//         const ws = new WebSocket('ws://localhost:8080');
-
-//         ws.onopen = () => console.log('WebSocket Connected');
-//         ws.onmessage = event => {
-//             const newData = JSON.parse(event.data);
-//             series?.update(newData[0]); // Utilisez la méthode update pour ajouter la nouvelle donnée
-//         };
-//         ws.onerror = error => console.error('WebSocket Error:', error);
-//         ws.onclose = () => console.log('WebSocket Disconnected');
-
-//         return () => ws.close();
-//     }, [series]); // Ajoutez series comme dépendance pour s'assurer qu'elle est définie
-
-//     return (
-//         <div>
-//             {showToolbar && <ToolBarChart />}
-//             <ChartContainer ref={chartContainerRef} />
-//         </div>
-//     );
-// }
-
-// export default Chart;
