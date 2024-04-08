51a4d5eeb21059ab45889d77966bb456b550a381
Crackvignoule <kiki.kalagan@gmail.com>
2024-04-08 14:33:17 +0000

    added ranking icons

--- client/src/components/Ranking/Ranking.js
+++ client/src/components/Ranking/Ranking.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { LeaderboardData } from "../../constants/constants";
 import { Tr, Td, StyledTable } from "./Ranking.styles";
-import { BsFillHexagonFill } from "react-icons/bs";
+import Rank from "./Rank/Rank";
 
 function Ranking() {
   return (
@@ -18,10 +18,7 @@ function Ranking() {
           <tbody>
           {LeaderboardData.map((data) => (
             <Tr key={data.rank}>
-              <div style={{ position: 'relative', display: 'inline-flex', justifyContent: 'center', alignItems: 'center' }}>
-                <BsFillHexagonFill color={data.rank <= 3 ? 'gold' : 'white'} />
-                <span style={{ position: 'absolute' }}>{data.rank}</span>
-              </div>
+              <Rank rank={data.rank} />
               <td>{data.username}</td>
               <td>{data.totalValue}</td>
               <Td className={data.evolution.startsWith('+') ? 'positive' : 'negative'}>{data.evolution}</Td>
