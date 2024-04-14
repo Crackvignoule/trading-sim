af85ed8903daa3fd150f684d07dd840f0e4c5bfd
Slynax <remlouplay3@gmail.com>
2024-04-14 22:32:33 +0200

    Update user balance display in NavBar component

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -18,6 +18,7 @@ function NavBar() {
   const navigate = useNavigate();
   const [pseudo, setPseudo] = useState("");
   const [logout, setLogout] = useState("Login");
+  const [balance, setBalance] = useState(0);
 
   const handleClick = (event) => {
     const { offsetWidth, offsetLeft } = event.target;
@@ -31,6 +32,64 @@ function NavBar() {
       setUnderlineStyle({ width: offsetWidth, left: offsetLeft });
     }
   }, []);
+
+  useEffect(() => {
+    const ws4 = new WebSocket('ws://localhost:8787');
+    const userToken = localStorage.getItem('token');
+    ws4.onopen = () => {
+        console.log('Connexion WebSocket4 établie');
+        // Envoyer le token de l'utilisateur juste après l'établissement de la connexion
+        ws4.send(JSON.stringify({ type: 'registration', token: userToken }));
+    };
+
+    // Écouter les messages entrants
+    ws4.onmessage = (event) => {
+      setBalance(JSON.parse(event.data).userSolde);
+    
+      };
+
+      // Nettoyer en fermant la connexion WebSocket quand le composant se démonte
+      return () => {
+          ws4.close();
+      };
+    }, []);
+
+
+  useEffect(() => {
+  const getUserSolde = async () => {
+    // get user soldes
+    const userToken = localStorage.getItem('token');
+    try {
+      const response = await fetch('http://localhost:5000/api/get-user-solde', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ userToken: userToken }),
+      });
+      const data = await response.json();
+      if (response.status === 200) {
+        return { result: true, value: data.amount };
+      } else {
+        console.log('Échec de la connexion');
+        return { result: false };
+      }
+    } catch (error) {
+      console.error('Erreur lors de la connexion', error);
+      return { result: false };
+    }
+  };
+
+  getUserSolde().then((result) => {
+    if (result.result) {
+      setBalance(result.value);
+    } else {
+      console.log('error');
+    }
+  });
+}, []);
+
+
 
   useEffect(() => {
       const token = localStorage.getItem('token');
@@ -72,7 +131,7 @@ function NavBar() {
         </List>
         <Underline $width={underlineStyle.width} $left={underlineStyle.left} />
         <RightSideDiv>
-          <LabelBalance>Balance : 14524 USDT</LabelBalance>
+          <LabelBalance>Balance : {balance} USDT</LabelBalance>
           <Label>{pseudo}</Label>
           <Button onClick={() => handleLogout()}>{logout}</Button>
         </RightSideDiv>
--- server/models/userWallets.js
+++ server/models/userWallets.js
@@ -2,7 +2,6 @@ const db = require("../database");
 
 async function getTokenAmountByUser(pseudo, tokenName) {
     try {
-      // Construisez votre requête SQL pour joindre les tables Users et Wallets
       const query = `
         SELECT W.tokenName, W.amount
         FROM Wallets W
@@ -10,10 +9,8 @@ async function getTokenAmountByUser(pseudo, tokenName) {
         WHERE U.pseudo = ? AND W.tokenName = ?
       `;
       
-      // Exécutez la requête avec les paramètres fournis
       const [rows] = await db.query(query, [pseudo, tokenName]);
       
-      // Vérifiez si nous avons un résultat
       if (rows.length > 0) {
         return { success: true, data: rows[0] }; // Retourne le premier résultat trouvé
       } else {
@@ -67,6 +64,127 @@ async function getTokenAmountByUser(pseudo, tokenName) {
     }
   }
   
-  
 
-module.exports = { getTokenAmountByUser, setUserWallet };
+  async function getUserSolde(userToken) {
+    try {
+      
+      const query = `
+        SELECT tokenName, amount FROM Wallets WHERE idUser IN (SELECT idUser FROM Users WHERE userToken = ?);
+      `;
+      
+      // Exécutez la requête avec les paramètres fournis
+      const [results] = await db.query(query, [userToken]);
+
+      let userSolde = 0;
+
+      if (results.length > 0) {
+        const promises = results.map(async token => {
+
+          if (token.tokenName === "USDT") {
+            userSolde += token.amount;
+          }
+          else{
+
+          const namePair = token.tokenName + "/USDT";
+          const queryPrice = `
+          SELECT currentPrice FROM PricesHistory ph
+          INNER JOIN Pairs p ON p.idPair = ph.idPair
+          WHERE p.namePair = ? ORDER BY ph.datePrice DESC LIMIT 1;`;
+          
+          const [resultPrice] = await db.query(queryPrice, [namePair]);
+          userSolde += resultPrice[0].currentPrice * token.amount; 
+        }
+  
+        });
+
+        await Promise.all(promises);
+
+        userSolde = userSolde.toFixed(4);
+        return { success: true, data: userSolde }; // Retourne le premier résultat trouvé
+      } else {
+        return { success: false, message: "Aucune donnée trouvée pour cet utilisateur et ce token." };
+      }
+      
+    } catch (error) {
+      console.error("Erreur lors de la récupération des données :", error);
+      return { success: false, message: "Erreur lors de la récupération des données." };
+    }
+  }
+  
+  async function getAllUserSolde(clientsSoldeToken) {
+    
+    if(Object.keys(clientsSoldeToken).length !== 0) {
+      try {
+        const query = `
+            SELECT U.userToken, W.tokenName, W.amount
+            FROM Wallets W
+            JOIN Users U ON W.idUser = U.idUser
+            WHERE U.userToken IN (?);
+        `;
+      
+        // Exécutez la requête avec les paramètres fournis
+        const [results] = await db.query(query,[Object.keys(clientsSoldeToken)]);
+
+        let userSoldes = {};
+
+        // Get the latest prices for all pairs
+        const queryPrice = `
+            SELECT p.namePair, ph.currentPrice
+            FROM Pairs p
+            INNER JOIN PricesHistory ph ON p.idPair = ph.idPair
+            INNER JOIN (
+                SELECT p.namePair, MAX(ph.datePrice) AS maxDate
+                FROM PricesHistory ph
+                INNER JOIN Pairs p ON p.idPair = ph.idPair
+                GROUP BY p.namePair
+            ) subq ON p.namePair = subq.namePair AND ph.datePrice = subq.maxDate;
+        `;
+        const [prices] = await db.query(queryPrice);
+
+        // Convert the prices array to an object for easier lookup
+        let pricesObj = {};
+        prices.forEach(price => {
+            pricesObj[price.namePair] = price.currentPrice;
+        });
+
+        if (results.length > 0) {
+            results.forEach(token => {
+                if (!userSoldes[token.userToken]) {
+                    userSoldes[token.userToken] = 0;
+                }
+
+                if (token.tokenName === "USDT") {
+                    userSoldes[token.userToken] += token.amount;
+                } else {
+                    const namePair = token.tokenName + "/USDT";
+                    userSoldes[token.userToken] += pricesObj[namePair] * token.amount; 
+                }
+            });
+
+            // Convert each userSolde to a fixed decimal number
+            for (let userToken in userSoldes) {
+                userSoldes[userToken] = userSoldes[userToken].toFixed(4);
+            }
+
+            // Convert the userSoldes object to an array of objects
+            const userSoldesArray = Object.keys(userSoldes).map(userToken => ({
+                userToken: userToken,
+                userSolde: userSoldes[userToken]
+            }));
+
+            return { success: true, data: userSoldesArray }; // Return the userSoldes array
+        } else {
+            return { success: false, message: "Aucune donnée trouvée pour ces utilisateurs et ces tokens." };
+        }
+      
+      } catch (error) {
+        console.error("Erreur lors de la récupération des données :", error);
+        return { success: false, message: "Erreur lors de la récupération des données." };
+      }
+    }
+    else {
+      return { success: false, message: "Aucun utilisateur enregistré sur wss4." };
+    }
+}
+
+module.exports = { getTokenAmountByUser, setUserWallet, getUserSolde, getAllUserSolde };
